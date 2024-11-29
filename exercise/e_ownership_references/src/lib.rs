dfd03a86d00c10ad4bf239b58267a230235911fe
dmp <kolapsys@yahoo.com>
2024-11-29 17:06:43 +0200

    Solve ex. e

---
+++ exercise/e_ownership_references/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn inspect(s: &String) {
+    if s.ends_with("s") {
+        println!("{} is plural.", s);
+    } else {
+        println!("{} is singluar.", s);
+    }
+}
+
+pub fn change(s: &mut String) {
+    if !s.ends_with("s") {
+        s.push_str("s");
+    }
+}
+
+pub fn eat(s: String) -> bool {
+    if s.starts_with("b") && s.contains("a") {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn bedazzle(s: &mut String) {
+    *s = String::from("sparkly");
+}
