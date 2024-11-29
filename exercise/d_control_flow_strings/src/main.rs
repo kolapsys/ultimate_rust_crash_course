28404dbc84f620eaacc4fd2b3adb2e72067253fb
dmp <kolapsys@yahoo.com>
2024-11-29 17:16:25 +0200

    Bring instruction comments back

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -2,9 +2,27 @@
 #![allow(dead_code, unused_mut, unused_variables)]
 
 fn main() {
+    // This collects any command-line arguments into a vector of Strings.
+    // For example:
+    //
+    //     cargo run apple banana
+    //
+    // ...produces the equivalent of
+    //
+    //     vec!["apple".to_string(), "banana".to_string()]
     let args: Vec<String> = std::env::args().skip(1).collect();
 
+    // This consumes the `args` vector to iterate through each String
     for arg in args {
+        // 1a. Your task: handle the command-line arguments!
+        //
+        // - If arg is "sum", then call the sum() function
+        // - If arg is "double", then call the double() function
+        // - If arg is anything else, then call the count() function, passing "arg" to it.
+
+
+        // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
+        // after "cargo run".  For example "cargo run sum"
         if arg == "sum" {
             sum();
         } else if arg == "double" {
@@ -17,6 +35,10 @@ fn main() {
 
 fn sum() {
     let mut sum = 0;
+    // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
+    // and add them all together (increment the `sum` variable).  Hint: You should get 255
+    // Run it with `cargo run sum`
+
     for i in 7..=23 {
         sum += i;
     }
@@ -27,6 +49,10 @@ fn sum() {
 fn double() {
     let mut count = 0;
     let mut x = 1;
+    // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
+    // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
+    // with `cargo run double`  Hint: The answer is 9 times.
+
     while x < 500 {
         x *= 2;
         count += 1;
@@ -38,9 +64,12 @@ fn double() {
 fn count(arg: String) {
 
     let mut count = 0;
+    // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
+    // You will need to count your loops, somehow.  Run it with `cargo run bananas`
+    //
     loop {
         count += 1;
-        print!("{} ", arg);
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
         if count == 8 { break; }
     }
 
